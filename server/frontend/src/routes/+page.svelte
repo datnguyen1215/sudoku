bdca9f16e538628ce62cad14ed2e369174dd5d5f
Dat Nguyen <datnguyen1215@gmail.com>
2025-08-01 05:10:08 -0700

    boilerplate for frontend.

---
+++ server/frontend/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+This is a normal working page. Nothing here yet.
---
+++ server/frontend/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $components: 'src/lib/components',
+      $utils: 'src/lib/utils',
+      $types: 'src/lib/types'
+    }
+  }
+};
+
+export default config;
