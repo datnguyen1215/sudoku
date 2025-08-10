98b9c9d7e33f38b52e306008d5cdb01099ff7acb
Dat Nguyen <datnguyen1215@gmail.com>
2025-08-10 07:52:13 -0700

    clear project.

--- mobile/android/app/src/main/java/com/sudoku/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.sudoku.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- server/frontend/src/lib/utils/gameSession.js
+++
@@ -1,9 +0,0 @@
-/**
- * Game session management utilities - main export file
- */
-
-// Re-export all utilities from the organized modules
-export { createGameSession, loadGameSession } from './gameSession/sessionManager.js';
-export { saveGameSession, saveGameSessionLocally } from './gameSession/saveStrategies.js';
-export { getFilledNumbersInRow, getFilledNumbersInColumn, getFilledNumbersInBox } from './gameSession/gridHelpers.js';
-export { formatTime } from './formatters.js';
